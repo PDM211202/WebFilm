2bb0e2720fe0975780e90e4080d1d5f862c415fa
PDM211202 <phodinhmanh2002@gmail.com>
2023-11-14 17:08:53 +0700

    update

--- server/app.js
+++ server/app.js
@@ -147,7 +147,7 @@ const admin = require("./routers/admin.router");
 const movie = require("./routers/movie.router");
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://web-film-gamma.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
